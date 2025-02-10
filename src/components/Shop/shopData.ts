aaa6b56e57f9326f0a16e8df941e1f677bf8eec8
glennhkm <glenn@mhs.usk.ac.id>
2025-02-11 00:37:35 +0700

    init project

---
+++ src/components/Shop/shopData.ts
@@ -0,0 +1,141 @@
+import { Product } from "@/types/product";
+const shopData: Product[] = [
+  {
+    title: "Padi",
+    reviews: 15,
+    price:30000,
+    discountedPrice:30000,
+    id: 1,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 5,
+    price:30000,
+    discountedPrice:30000,
+    id: 2,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 5,
+    price:30000,
+    discountedPrice:30000,
+    id: 3,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 6,
+    price:30000,
+    discountedPrice:30000,
+    id: 4,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 3,
+    price:30000,
+    discountedPrice:30000,
+    id: 5,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 15,
+    price:30000,
+    discountedPrice:30000,
+    id: 6,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 15,
+    price:30000,
+    discountedPrice:30000,
+    id: 7,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+  {
+    title: "Padi",
+    reviews: 15,
+    price:30000,
+    discountedPrice:30000,
+    id: 8,
+    imgs: {
+      thumbnails: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+      previews: [
+        "/images/products/padi.png",
+        "/images/products/padi.png",
+      ],
+    },
+  },
+];
+
+export default shopData;
